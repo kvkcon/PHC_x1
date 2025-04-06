c3017f8162d2561167e79a888313af101a012e21
kvkcon <2300271042@email.szu.edu.cn>
2025-04-06 10:46:44 +0800

    fix: change file dir path

--- scripts/vis/mujocoxml2urdf.py
+++ scripts/vis/mujocoxml2urdf.py
@@ -1,4 +1,4 @@
 from mjcf_urdf_simple_converter import convert
 
-convert("x1.xml", "x1_awr.urdf", asset_file_prefix="package://x1_urdf/armwalker_mujoco_xml/meshes/")
-convert("x1.xml", "x1_aw.urdf")
+convert("phc/data/assets/robot/zhiyuan_x1/x1.xml", "phc/data/assets/robot/zhiyuan_x1/urdf/x1_awr.urdf", asset_file_prefix="package://x1_urdf/armwalker_mujoco_xml/meshes/")
+convert("phc/data/assets/robot/zhiyuan_x1/x1.xml", "phc/data/assets/robot/zhiyuan_x1/urdf/x1_aw.urdf")
